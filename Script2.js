bdf30d450d353998d83e3dc849904abebd373f11
Okrash0 <35527883+Okrash0@users.noreply.github.com>
2018-04-25 13:58:21 +0200

    Update Script2.js

--- Script2.js
+++ Script2.js
@@ -163,4 +163,11 @@ function myFunction(){
     d20();
   }
   
+  if (key == 81){d12();}
+  else if (key == 87){d10();}
+  else if (key == 69){d8();}
+  else if (key == 82){d6();}
+  else if (key == 84){d4();}
+    
+  
 }
